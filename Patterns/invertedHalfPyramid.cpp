815d33bef81ea5e42bfe212ae67a2c1f3f22cb2b
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-07-03 14:14:55 +0530

    added code for 180 degrees rotation

--- Patterns/invertedHalfPyramid.cpp
+++ Patterns/invertedHalfPyramid.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 
 int main(){
-    int n;
-    cout<<"Enter the value of n: ";
+    int m,n;
+    
+    cout<<"Enter the height for inverted pyramid: ";
     cin>>n;
     while(n!=0){
         for (int i = n; i >0; i--)
@@ -16,6 +17,26 @@ int main(){
         cout<<endl;
         n--;
     }
+
+    cout<<endl<<endl;
     
+    // FOR INVERTED PYRAMID ROTATED BY -180 DEGREES
+    int h;
+	cout<<"Enter the height of pyramid"<<endl;
+	cin>>h;
+	
+	for(int i=1; i<=h; i++)
+	{
+        for(int j=1; j<=h; j++)
+        {
+        	if(j<=h-i)
+        	{
+        		cout<<"  ";
+			}
+			else cout<<" #";
+		}
+		cout<<endl;
+	}
+
     return 0;
 }
