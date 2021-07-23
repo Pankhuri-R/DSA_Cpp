de7185759fe811ef6b60ead9ba03148dd8f26882
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-07-23 22:26:13 +0530

    adding output statement

--- Patterns/star.cpp
+++ Patterns/star.cpp
@@ -6,6 +6,7 @@ int main(){
     int n;
     cout<<"Enter n value: "<<endl;
     cin>>n;
+    cout<<"Printing..."<<endl;
     for(int i=1; i<= n ; i++){
         for(int j=1 ; j<= n-i; j++){
             cout<<" ";
