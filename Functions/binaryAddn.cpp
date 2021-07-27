9a98f3ac397189d7fd0e58d5a59b04bd3381b5fd
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-07-27 19:45:08 +0530

    addition of 2 binary numbers

---
+++ Functions/binaryAddn.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int reverse(int n){
+    int rev=0;
+    while (n>0)
+    {
+        int lastDigit = n%10;
+        rev= rev*10 + lastDigit;
+        n /= 10;
+    }
+    return rev;
+}
+
+int add(int a, int b){
+    int ans= 0;
+    int prevCarry=0;
+
+    while (a>0 && b>0)
+    {
+        if (a%2==0 && b%2==0)
+        {
+            ans= ans*10 + prevCarry;
+            prevCarry=0;
+        }
+        else if ((a%2==0 && b%2==1) || (a%2==1 && b%2==0))
+        {
+            if (prevCarry == 1)
+            {
+                ans= ans*10 + 0 ;
+                prevCarry= 1;
+            }
+            else{
+                ans= ans*10 + 1;
+                prevCarry=0;
+            }
+        }
+        else{
+            ans= ans*10 + prevCarry;
+            prevCarry=1;
+        }
+        a /= 10;
+        b /= 10;
+    }
+
+    while (a>0)
+    {
+        if (prevCarry==1)
+        {
+            if (a%2 == 1)
+            {
+                ans = ans*10 + 0;
+                prevCarry=1;
+            }
+            else{
+                ans = ans*10 + 1;
+                prevCarry=0;
+            }
+        }
+        else{
+            ans = ans*10 + (a%2);
+            prevCarry=0;
+        }
+        a /= 10;
+    }
+
+    while (b>0)
+    {
+        if (prevCarry==1)
+        {
+            if (b%2 == 1)
+            {
+                ans = ans*10 + 0;
+                prevCarry=1;
+            }
+            else{
+                ans = ans*10 + 1;
+                prevCarry=0;
+            }
+        }
+        else{
+            ans = ans*10 + (b%2);
+            prevCarry=0;
+        }
+        b /= 10;
+    }
+
+    if (prevCarry == 1)
+    {
+        ans = ans*10 + 1;
+    }
+    return reverse(ans);
+}
+
+int main() {
+    int a,b;
+    cout<<"Enter both the numbers one by one\n";
+    cin>>a>>b;
+
+    cout<<add(a,b);
+    return 0;
+}
