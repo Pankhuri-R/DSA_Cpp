27bb1b7d2c7386252c80df9f19504ad3a32cac65
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-07-27 21:34:20 +0530

    binary search in array

---
+++ Arrays/Searching/binary.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int binarySearch(int arr[],int n, int key){
+    int start=0;
+    int end= n-1;
+    while (start<=end)
+    {
+        int mid= (start+end)/2;
+        if (arr[mid]>key)
+        {
+            end= mid-1;
+        }
+        else if (arr[mid]<key)
+        {
+            start=mid+1;
+        }
+        else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n,key;
+    cout<<"Enter size of array\n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter key: ";
+    cin>>key;
+    cout<<key<<" is found at index "<<binarySearch(arr,n,key);
+    return 0;
+}
