7374855398cedf89acb55c7251cc6816cfdd7539
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-08-06 22:56:38 +0530

    Transpose of matrix

---
+++ Arrays/2DArrays/transpose.cpp
@@ -0,0 +1,19 @@
+#include<bits\stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,m;
+    cout<<"Enter rows and columns resp: ";
+    cin>>n>>m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    return 0;
+}
