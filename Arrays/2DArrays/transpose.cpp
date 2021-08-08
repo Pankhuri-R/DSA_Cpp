e12b21cd7d6da27e2d7942c15bad6d7b1b765f38
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-08-08 10:15:30 +0530

    transpose of matrix

--- Arrays/2DArrays/transpose.cpp
+++ Arrays/2DArrays/transpose.cpp
@@ -15,5 +15,26 @@ int main(){
         }
     }
 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < m; j++)
+        {
+            
+            int temp;
+            temp= arr[i][j];
+            arr[i][j]=arr[j][i];
+            arr[j][i]=temp;
+            
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
