ef2c367d23c6cdfd2224c92d21a5556331009f7d
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-08-15 14:09:41 +0530

    Converting string into upper & lowercase

--- Strings/upperLower.cpp
+++ Strings/upperLower.cpp
@@ -7,10 +7,21 @@ int main(){
 
     for (int i = 0; i < s1.size(); i++)
     {
-        s1[i]+= '32';
+        if(s1[i]>='a' && s1[i]<='z'){
+            s1[i]-= 32;
+        }
     }
 
-    cout<<s1<<endl;
+    cout<<"Uppercase:"<<s1<<endl;
+
+    for (int i = 0; i < s1.size(); i++)
+    {
+        if(s1[i]>='A' && s1[i]<='Z'){
+            s1[i]+= 32;
+        }
+    }
+
+    cout<<"Lowercase:"<<s1<<endl;
     
     return 0;
 }
