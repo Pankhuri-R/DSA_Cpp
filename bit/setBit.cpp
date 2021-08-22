6923c69e8aff7f91a272f5f226d3bd1d0e648970
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-08-22 21:16:54 +0530

    Clearing bit

--- bit/setBit.cpp
+++ bit/setBit.cpp
@@ -6,7 +6,12 @@ int setBit(int n,int pos){
     return (n | (1<<pos));
 }
 
+int clearBit(int n,int pos){
+    return (n&(~(1<<pos)));
+}
+
 int main(){
-    cout<<setBit(5,3)<<endl;
+    cout<<setBit(5,1)<<endl;
+    cout<<clearBit(7,1)<<endl;
     return 0;
 }
