3c9fb05f9e5e4b95fadee405b827640a01458a13
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-09-21 19:42:01 +0530

    Tower of Hanoi and removing duplicates from string

---
+++ Recursion/toh.cpp
@@ -0,0 +1,21 @@
+#include <bits\stdc++.h>
+
+using namespace std;
+
+void towerOfHanoi(int n, char src, char helper, char dstn)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    towerOfHanoi(n - 1, src, dstn, helper);
+    cout << "Move from " << src << " to " << dstn << endl;
+    towerOfHanoi(n - 1, helper, src, dstn);
+}
+
+int main()
+{
+    towerOfHanoi(3, 'A', 'B', 'C');
+    return 0;
+}
