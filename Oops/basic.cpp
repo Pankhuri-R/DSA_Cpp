850c7c2af05294506bb0e0b2a15000bafbc0cd61
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-10-12 20:18:42 +0530

    Basic class example of OOPs

---
+++ Oops/basic.cpp
@@ -0,0 +1,37 @@
+#include <bits\stdc++.h>
+
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int age;
+    bool gender;
+
+    void printInfo()
+    {
+        cout << "Name=" << name << endl;
+        cout << "Age=" << age << endl;
+        cout << "Gender=" << gender << endl;
+    }
+};
+
+int main()
+{
+    Student arr[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Name=";
+        cin >> arr[i].name;
+        cout << "Age= ";
+        cin >> arr[i].age;
+        cout << "Gender=";
+        cin >> arr[i].gender;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        arr[i].printInfo();
+    }
+}
